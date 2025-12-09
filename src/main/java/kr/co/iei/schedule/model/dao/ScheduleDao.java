867d56aa58cafe4c3d94ff77af2e726a1a314921
JeonHywn <wjsgusdn1tp@naver.com>
2025-12-09 13:29:29 +0900

    feat : 주석추가

--- src/main/java/kr/co/iei/schedule/model/dao/ScheduleDao.java
+++ src/main/java/kr/co/iei/schedule/model/dao/ScheduleDao.java
@@ -10,7 +10,10 @@ import kr.co.iei.schedule.model.dto.ScheduleDTO;
 public interface ScheduleDao {
 	// 일정 등록
     int insertSchedule(ScheduleDTO schedule);
+    // 일정 리스트
 	List<ScheduleDTO> selectScheduleList();
+	// 일정 수정
 	int updateSchedule(ScheduleDTO scheduleDTO);
+	// 일정 삭제
 	int deleteSchedule(int scheduleNo);
 }
